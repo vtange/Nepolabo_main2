120ed2ba5a65acd448faf28af7b72c9433c95170
Victor Tang <vtange@users.noreply.github.com>
2023-09-17 16:58:43 -0700

    service worker setup code

--- 2023/aqualamyum/presw.js
+++ 2023/aqualamyum/presw.js
@@ -46,10 +46,30 @@ var nojs = document.getElementById("nojs-cover");
 if(nojs){
     nojs.style.display = "none";
 }
-//WebGL
+// WebGL
 if(!check_webgl()) {
     return;
 }
 
-addBundleJS();
+// Webpack dev-server's inject_mode will add bundle for us. This function is just for prod.
+var inject = document.getElementById("inject");
+if(inject) {
+    return;
+}
+
+// If we include swhandler.js, this app uses service workers.
+if(typeof wait_for_svc_worker_setup === "function") {
+    // If zzzz is in URL, delete and skip service worker.
+    if(typeof window.location.href === "string" && window.location.href.indexOf("zzzz") !== -1) {
+        delete_old_service_workers();
+        addBundleJS();
+    } else {
+        wait_for_svc_worker_setup(0).then(function(rsVal){
+            addBundleJS();
+        });
+    }
+} else {
+    // No service worker mode
+    addBundleJS();
+}
 })();
--- 2023/aqualamyum/sw.js
+++ 2023/aqualamyum/sw.js
@@ -1,9 +1,12 @@
 importScripts("hashes.js");
-
+// Service worker is a simulated edge server. It caches files fetched online for offline use.
 function log(str) {
-    //console.log(str);
+    console.log(str);
 }
-var cacheName = 'v3';
+if(!hashes) {
+    log('[SvcWrker] NO HASHES');
+    return;
+}
 var cacheFiles = [
     './index.html',
     './' + hashes["vendors~main"]["js"],
@@ -15,24 +18,21 @@ if(cssPath)
 {
     cacheFiles.push('./'+cssPath);
 }
+var cacheName = hashes["vendors~main"]["js"]+hashes["main"]["js"]+hashes["main"]["css"];
 
 // Install event
 self.addEventListener('install', function(event) {
-    log("SW:INSTALL");
+    log('[SvcWrker] INSTALL');
     event.waitUntil(
         caches.open(cacheName)
         .then(function(cache){
-            log('SW caching:', cacheFiles, cacheName);
             return cache.addAll(cacheFiles).then(function(){
-                //finished initial file fetch
-                log("SW:INSTALL---> COMPLETE");
+                log("[SvcWrker] INSTALL---> COMPLETE");
             }).catch(function(err){
-                //error fetching file
                 throw err;
             });;
         })
         .catch(function(err){
-            console.error(err.message);
             throw err;
         })
     )
@@ -40,14 +40,14 @@ self.addEventListener('install', function(event) {
 
 // Activate event
 self.addEventListener('activate', function(event) {
-    log("SW:ACTIVATE");
+    log('[SvcWrker] ACTIVATE');
     event.waitUntil(
         caches.keys()
         .then(function(cacheNames){
-            return Promise.all(cacheNames.map(function(thisCacheName){
-                if(thisCacheName !== cacheName){
-                    log('SW Removing obsolete cached files from', thisCacheName);
-                    return caches.delete(thisCacheName);
+            return Promise.all(cacheNames.map(function(existingCacheName){
+                if(existingCacheName !== cacheName){
+                    log('[SvcWrker] CLEAR_OLD_CACHENAME:', existingCacheName);
+                    return caches.delete(existingCacheName);
                 }
             }))
         })
@@ -59,35 +59,46 @@ self.addEventListener('activate', function(event) {
 
 // Push event (for mobile push messages)
 self.addEventListener('push', function(event) {
-  log('Push message received', event);
+    if (!(self.Notification && self.Notification.permission === "granted")) {
+        log('[SvcWrker] PUSH_MSG, no permission');
+        return;
+    } else if (!event.data) {
+        log('[SvcWrker] PUSH_MSG, no data');
+        return;
+    }
+    const data = event.data.json() || {};
+    const title = data.title || "Test Push Notification Title";
+    const message =
+      data.message || "Test Push Notification Message.";
+    /*const notification = */new self.Notification(title, {
+      body: message,
+    //   tag: "simple-push-demo-notification",
+    //   icon: "images/new-notification.png",
+    });
+    // notification.addEventListener("click", () => {
+    //     clients.openWindow(
+    //       "https://example.blog.com/2015/03/04/something-new.html",
+    //     );
+    //   });
 });
 
 // Fetch event
 self.addEventListener('fetch', function(event) {
-    log("SW:FETCH", event.request.url);
+    log("[SvcWrker] FETCH:", event.request.url);
     event.respondWith(
         caches.match(event.request)
         .then(function(response){
-
-            //for hashes.js, default to network so we always get the freshest hashes
-            if(event.request.url.lastIndexOf("hashes") !== -1) response = undefined;
-
             if(response) {
-                log(event.request.url, '->Got Files from cache:' + response);
+                log("[SvcWrker] FETCH->CACHE_RESPONSE", url);
+            } else {
+                log("[SvcWrker] FETCH->NO_CACHE_FOUND", url);
             }
-            else {
-                log(event.request.url, '->Fetching new files:');
-            }
-
             return response || fetch(event.request).then(function(response){
-                //got file from fetch operation
-                log(event.request.url, '->Fetched:', response);
-                /*
-                    return caches.open(cacheName).then(function(cache) {
-                        cache.put(event.request, response.clone());
-                        return response;
-                    });
-                */
+                if(response) {
+                    log("[SvcWrker] FETCH->LIVEFETCH_RESPONSE", url);
+                } else {
+                    log("[SvcWrker] FETCH->NO_LIVEFETCH_RESPONSE", url);
+                }
                 return response;
             }).catch(function(err){
                 //error fetching file
